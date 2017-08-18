72f3dff606637579f69ecf655015efe216ef3462
aragamix01 <aragamix01@gmail.com>
2017-08-18 22:18:44 +0700

    dsd

--- bot2.php
+++ bot2.php
@@ -1,7 +1,8 @@
 <?php
-    // $token = 'C9ac0d8c426f5f88e9609cc2f5d8a23b8';
-    // $secret = 'd4f35b7ecb191edd8b6a48631d06780d';
-    // $cToken = '5fUwVGVAEzh0STff4waRo1361kbdfS306CJ+pVaO9+T7dXfrSLum6m0nAWpDx3hOKBVKPiebT7tVDcwT3MPSZYKiKYX0M2RSAgpDu2pFnjHw4YziX8CTgyyqZcgT39OjtOc+OatIxAfp/J+d/YTNRQdB04t89/1O/w1cDnyilFU=';
+    $roomToken = 'C9ac0d8c426f5f88e9609cc2f5d8a23b8';
+    $myToken = 'U44f0ae5516e30be72d074400a86ef343';
+    $secret = 'd4f35b7ecb191edd8b6a48631d06780d';
+    $cToken = '5fUwVGVAEzh0STff4waRo1361kbdfS306CJ+pVaO9+T7dXfrSLum6m0nAWpDx3hOKBVKPiebT7tVDcwT3MPSZYKiKYX0M2RSAgpDu2pFnjHw4YziX8CTgyyqZcgT39OjtOc+OatIxAfp/J+d/YTNRQdB04t89/1O/w1cDnyilFU=';
     // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($cToken);
     // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
 
@@ -9,5 +10,28 @@
     // $response = $bot->pushMessage($token, $textMessageBuilder);
 
     // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-    echo 'test';
+
+    $url = 'https://api.line.me/v2/bot/message/reply';
+
+    $messages = [
+				'type' => 'text',
+				'text' => test
+	];
+
+    $data = [
+        'to' => $myToken,
+        'messages' => [$messages],
+    ];
+    $post = json_encode($data);
+    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $cToken);
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result . "\r\n";
 ?>
