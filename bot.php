92e7481a15eaf5cd299c2a69429aedeea6fbb3bd
aragamix01 <aragamix01@gmail.com>
2017-08-18 20:31:05 +0700

    23

--- bot.php
+++ bot.php
@@ -1,8 +1,11 @@
 <?php
 $access_token = '	5fUwVGVAEzh0STff4waRo1361kbdfS306CJ+pVaO9+T7dXfrSLum6m0nAWpDx3hOKBVKPiebT7tVDcwT3MPSZYKiKYX0M2RSAgpDu2pFnjHw4YziX8CTgyyqZcgT39OjtOc+OatIxAfp/J+d/YTNRQdB04t89/1O/w1cDnyilFU=';
 
-// Make a POST Request to Messaging API to reply to sender
+
 			$url = 'https://api.line.me/v2/bot/message/reply';
+
+            $text = $event['message']['text'];
+			$replyToken = $event['replyToken'];
 
             $messages = [
 				'type' => 'text',
