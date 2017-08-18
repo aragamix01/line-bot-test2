19ea2bc721aeeaa1d39b14c2cf109445ae62f68b
aragamix01 <aragamix01@gmail.com>
2017-08-18 23:40:46 +0700

    ss

--- bot2.php
+++ bot2.php
@@ -19,16 +19,19 @@
     $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $cToken);
         $content = file_get_contents('php://input');
         $events = json_decode($content, true);
-        if(is_null($events)){
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            $result = curl_exec($ch);
-            curl_close($ch);
-            echo $result . "\r\n"; 
+        while(1){
+            if(is_null($events)){
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                $result = curl_exec($ch);
+                curl_close($ch);
+                echo $result . "\r\n"; 
+            }
+            sleep(15);
         }
         echo 'ok';
 ?>
