2c7fa89641f595f437979e0d133323afd783f1a8
aragamix01 <aragamix01@gmail.com>
2017-08-19 19:56:00 +0700

    ss

--- bot.php
+++ bot.php
@@ -12,9 +12,9 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$text = $event['message']['text'];
-            $scr = $event['source'];
-            $text = $scr['groupId'];
+			$text = $event['message']['text'];
+            //$scr = $event['source'];
+           //$text = $scr['groupId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
