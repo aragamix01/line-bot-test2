c16c85412a75d941d191cdcd0e5a714e14ad588b
aragamix01 <aragamix01@gmail.com>
2017-08-20 12:49:03 +0700

    cc

--- testConnection.php
+++ testConnection.php
@@ -16,7 +16,7 @@ $db = substr($url["path"], 1);
     if($conn->query($sql)){
         echo 'success';
         $qu = $conn->query("select * from heroku_da1dc32cdc85254.knowledge");
-        print_r($qu);
+        print_r(mysqli_fetch_assoc($qu));
     }else{
         echo 'failed';
     }
