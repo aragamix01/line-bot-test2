703740f5fe72345ad010791fc8ff180325191fe2
aragamix01 <aragamix01@gmail.com>
2017-08-20 14:44:56 +0700

    vv

--- bot3.php
+++ bot3.php
@@ -1,10 +1,11 @@
 <?php
-url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
+
 
 if($conn = new mysqli($server, $username, $password, $db)){
     echo true;
