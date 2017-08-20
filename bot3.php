484531dadfeca7bcc888d2c465fa9b135af82712
aragamix01 <aragamix01@gmail.com>
2017-08-20 15:36:53 +0700

    cc

--- bot3.php
+++ bot3.php
@@ -13,4 +13,53 @@ if($conn = new mysqli($server, $username, $password, $db)){
     echo false;
 }
 
+function curl_send(){
+
+}
+
+$access_token = '5fUwVGVAEzh0STff4waRo1361kbdfS306CJ+pVaO9+T7dXfrSLum6m0nAWpDx3hOKBVKPiebT7tVDcwT3MPSZYKiKYX0M2RSAgpDu2pFnjHw4YziX8CTgyyqZcgT39OjtOc+OatIxAfp/J+d/YTNRQdB04t89/1O/w1cDnyilFU=';
+$roomToken = 'C9ac0d8c426f5f88e9609cc2f5d8a23b8';
+
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+
+if (!is_null($events['events'])) {
+    foreach ($events['events'] as $event) {
+        if ( $event['type'] == 'message' && $event['message']['type'] == 'text' ){
+            $msg_status = 0; //0 = normal_message,1 = train_message,2 = power_message
+            if(strpos($event['message']['text'],"#?" !== false ){
+                $text = "ช้อนรู้แล้วว";
+                $msg_status = 1;
+            }
+
+            $replyToken = $event['replyToken'];
+
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
+
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result . "\r\n";
+        }
+    }
+}
+
+$conn->close();
+echo "OK";
 ?>
