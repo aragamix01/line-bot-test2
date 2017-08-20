8cee2f816c3c34881cd83563bf4ce3434bf66189
aragamix01 <aragamix01@gmail.com>
2017-08-20 13:19:29 +0700

    cc

--- testConnection.php
+++ testConnection.php
@@ -17,6 +17,8 @@ $db = substr($url["path"], 1);
         echo 'success';
         $qu = $conn->query("select * from heroku_da1dc32cdc85254.knowledge");
       
+        echo type_of($qu);
+
         //print_r($qu);
         while ($row = mysql_fetch_array($qu, MYSQL_ASSOC)) {
             printf("Key: %s  Ans: %s", $row["key"], $row["ans"]);
