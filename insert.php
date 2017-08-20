050895d9ec37c41d1fb5b589891e425f0083ad1d
aragamix01 <aragamix01@gmail.com>
2017-08-20 12:44:29 +0700

    cc

---
+++ insert.php
@@ -0,0 +1,5 @@
+<form action="testConnection.php" method="POST">
+    <input type="text" name="key">
+    <input type="text" name="ans">
+    <input type="submit" name="submit" value="submit">
+</form>
--- testConnection.php
+++ testConnection.php
@@ -7,9 +7,15 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 
-    if($conn = new mysqli($server, $username, $password, $db)){
-        echo $server.' '.$username.' '.$password.' '.$db;
+   $conn = new mysqli($server, $username, $password, $db);
+
+    $key = $_POST["key"];
+    $ans = $_POST["ans"];
+    $sql = "INSERT INTO `heroku_da1dc32cdc85254`.`knowledge`(`key`,`ans`) VALUES ('$key','$ans')";
+    
+    if($conn->query($sql)){
+        echo 'success';
     }else{
-        echo false;
+        echo 'failed';
     }
 ?>
