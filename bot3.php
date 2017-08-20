aea0f417f222cd72da748e2556281c2ad24a9a72
aragamix01 <aragamix01@gmail.com>
2017-08-20 14:42:48 +0700

    cc

--- bot3.php
+++ bot3.php
@@ -1,18 +1,15 @@
 <?php
-// url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-
-// $conn = new mysqli($server, $username, $password, $db);
-
-function testFnc(){
-    echo 'eiei';
+if($conn = new mysqli($server, $username, $password, $db)){
+    echo true;
+}else{
+    echo false;
 }
-
-testFnc();
 
 ?>
