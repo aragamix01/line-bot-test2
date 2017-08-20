5712519a968c236763d3d36ea216599dc423c48f
aragamix01 <aragamix01@gmail.com>
2017-08-20 14:20:20 +0700

    cc

--- insert.php
+++ insert.php
@@ -12,18 +12,18 @@
     $db = substr($url["path"], 1);
     
     
-    //$conn = new mysqli($server, $username, $password, $db);
-    $conn = mysql_connect($server,$username,$password);
+    $conn = new mysqli($server, $username, $password, $db);
+    //$conn = mysql_connect($server,$username,$password);
 
-    if (!$conn) {
-        echo "Unable to connect to DB: " . mysql_error();
-        exit;
-    }
+    // if (!$conn) {
+    //     echo "Unable to connect to DB: " . mysql_error();
+    //     exit;
+    // }
     
-    if (!mysql_select_db($db)) {
-        echo "Unable to select mydbname: " . mysql_error();
-        exit;
-    }
+    // if (!mysql_select_db($db)) {
+    //     echo "Unable to select mydbname: " . mysql_error();
+    //     exit;
+    // }
     
     // $qu = $conn->query("select * from heroku_da1dc32cdc85254.knowledge");
     // print_r($qu);
