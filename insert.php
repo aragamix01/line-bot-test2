f9e4322a125c287393e130cd0dd4c9729843f113
aragamix01 <aragamix01@gmail.com>
2017-08-22 12:23:04 +0700

    dd

--- insert.php
+++ insert.php
@@ -1,3 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
 <?php 
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     
