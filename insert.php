72c36432626dd7768e4d5931cbee2a5fb77515d8
aragamix01 <aragamix01@gmail.com>
2017-08-22 12:48:32 +0700

    cc

--- insert.php
+++ insert.php
@@ -4,7 +4,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </head>
     <body>
-    <nav class="navbar navbar-toggleable-md navbar-light" style="background-color: red;">
+    <nav class="navbar navbar-toggleable-md navbar-inverse" style="background-color: red;">
         <a class="navbar-brand pull-middle" href="#">Manage Bot Data</a>
     </nav>
     </body>
@@ -26,7 +26,7 @@
     $sql_select = "select * from heroku_da1dc32cdc85254.knowledge";
     if ($result = $conn->query($sql_select)) {
         echo '<table class="table">';
-        echo '<th><td>Delete</td><td>Keywords</td><td>Answers</td></th>';
+        echo '<th> <td>Delete</td> <td>Keywords</td> <td>Answers</td> </th>';
             while ($obj = $result->fetch_object()) {
                 echo '<tr>';
                     echo '<td><a href="delete.php?id='.$obj->knId.'" role="button" class="btn btn-danger">del</a></td>';
