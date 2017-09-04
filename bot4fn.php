97574e08b9558f82aba2f75cafc84ced2272d956
aragamix01 <aragamix01@gmail.com>
2017-09-04 09:42:50 +0700

    bot4

---
+++ bot4fn.php
@@ -0,0 +1,17 @@
+<?php
+    function getConnection(){
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+        
+        
+        if($conn = new mysqli($server, $username, $password, $db)){
+            echo true;
+        }else{
+            echo false;
+        }
+    }
+?>
