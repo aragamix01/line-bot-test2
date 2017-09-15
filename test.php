1328bb74cac91a62b60b71d8eff1c7f2646c7e6e
aragamix01 <aragamix01@gmail.com>
2017-09-15 23:39:32 +0700

    cc

--- test.php
+++ test.php
@@ -1,16 +1,16 @@
 <?php
 $messages = [
-    {
+    [
         'type' => 'text',
         'text' => $text
-    },
-    {
+    ],
+    [
         // 'type' => 'image',
         // 'originalContentUrl' => $imageUrl,
         // 'previewImageUrl' => $imageMiniUrl
         'type' => 'text',
         'text' => $text
-    }
+    ]
 ];
 
 echo 'Is a '.gettype($messages);
