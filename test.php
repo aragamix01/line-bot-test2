c629ef05709e2c50a6a96c057a22cbabad9523db
aragamix01 <aragamix01@gmail.com>
2017-09-15 22:55:48 +0700

    checkArray

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
 $messages = [
-    'msg1' => [
+    [
         'type' => 'image',
         'originalContentUrl' => $imageUrl,
         'previewImageUrl' => $imageMiniUrl
     ],
-    'msg2' => [
+    [
         'type' => 'image',
         'originalContentUrl' => $imageUrl,
         'previewImageUrl' => $imageMiniUrl
